4d93c863e64791f470f970d033e42d8b50441c5c
Roshankumar350 <roshankumar350@gmail.com>
2025-06-28 11:03:51 +0530

    project creation

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by Nidhi Kumari on 28/06/25.
+//
+
+import SwiftUI
+// API KEY:  0e3926db710647aba61d0bbdec88d920
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
